0362a8f4f27ecfa06617ac3b011c29a93b055bca
Nikita Masych <nikitamasich152@gmail.com>
2024-11-06 15:41:47 +0200

    feat: added source commands

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ wasm-pack build --target web
 ```bash
 npx http-server . -o
 ```
+
+## Data 
+
+You can see how `proof_with_public_inputs.bin` and `verifier_data.bin` are composed in `src/bin`
+
+Launch this command to end-to-end circuit launch; output the needed data in files:
+
+```bash
+cargo run --bin factorial
+```
+
+Launch this command to see how verification only happens using deserialized info.
+
+```bash
+cargo run --bin verify
+```
---
+++ src/bin/verify.rs
@@ -0,0 +1,21 @@
+use plonky2::plonk::circuit_data::VerifierCircuitData;
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::plonk::proof::ProofWithPublicInputs;
+use plonky2::util::serialization::DefaultGateSerializer;
+
+fn main() {
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+
+    let proof_bytes = std::fs::read("proof_with_public_inputs.bin").unwrap();
+    let verifier_data_bytes = std::fs::read("verifier_data.bin").unwrap();
+
+    let verifier = VerifierCircuitData::<F, C, D>::from_bytes(verifier_data_bytes, &DefaultGateSerializer).unwrap();
+
+    let proof = ProofWithPublicInputs::<F, C, D>::from_bytes(proof_bytes, &verifier.common).unwrap();
+
+    verifier.verify(proof).unwrap();
+
+    println!("Verified!");
+}
